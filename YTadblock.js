9c5a800a4b4b9eb644486b27a1432abdc1cbd3e9
beigeworm <93350544+beigeworm@users.noreply.github.com>
2023-10-21 11:18:20 +0100

    Update YTadblock.js

--- YTadblock.js
+++ YTadblock.js
@@ -6,8 +6,8 @@
 // @author       JoelMatic
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @updateURL    https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
-// @downloadURL  https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
+// @updateURL    https://raw.githubusercontent.com/beigeworm/assets/main/YTadblock.js
+// @downloadURL  https://raw.githubusercontent.com/beigeworm/assets/main/YTadblock.js
 // @grant        none
 // ==/UserScript==
 
