921327866e2fa29872c9fc8d67e74485283bb913
egieb <93350544+beigeworm@users.noreply.github.com>
2024-03-27 20:36:41 +0000

    Add files via upload

---
+++ decrypt.ps1
@@ -0,0 +1,16 @@
+﻿
+# $key = Read-Host "Enter the keyphrase "
+# $enc = Read-Host "Enter encrypted message "
+
+function Decipher-Text {
+    param ([string]$encodedText,[string]$key)
+    $decodedText = [System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($encodedText))
+    $result = ""
+    for ($i = 0; $i -lt $decodedText.Length; $i++) {
+        $charCode = [int]$decodedText[$i] -bxor [int]$key[$i % $key.Length]
+        $result += [char]$charCode
+    }
+    return $result
+}
+
+$global:decrypted = Decipher-Text -encodedText $enc -key $key
